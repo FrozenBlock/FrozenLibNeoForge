d4714e5e49c6ab34efc657f3d9806415d88f6c90
LiukRast <darthlucavader@gmail.com>
2024-09-09 11:35:33 +0200

    1.0.1

--- src/main/java/net/frozenblock/lib/networking/PlayerLookup.java
+++ src/main/java/net/frozenblock/lib/networking/PlayerLookup.java
@@ -1,13 +1,21 @@
 package net.frozenblock.lib.networking;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 
 public class PlayerLookup {
     public static Collection<ServerPlayer> all(MinecraftServer server) {
         return Collections.unmodifiableCollection(server.getPlayerList().getPlayers());
+    }
+
+    public static Collection<ServerPlayer> tracking(BlockEntity blockEntity) {
+        return ((ServerLevel) Objects.requireNonNull(blockEntity.getLevel())).getChunkSource().chunkMap.getPlayers(new ChunkPos(blockEntity.getBlockPos()), false);
     }
 }
